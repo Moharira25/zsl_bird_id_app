5cefb1897b53dbdcf363c68eabf3d9628673b7c7
Mohammed Abuharira <maya2@student.le.ac.uk>
2024-08-24 00:08:14 +0100

    Enhance WebSocket configuration with improved comments and URL pattern
    - Added Javadoc comments to the configuration class for better understanding.
    - Updated WebSocket URL pattern to use a path variable for clarity.

--- src/main/java/com/zsl_birdid/websocket/WebSocketConfig.java
+++ src/main/java/com/zsl_birdid/websocket/WebSocketConfig.java
@@ -1,18 +1,25 @@
 package com.zsl_birdid.websocket;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+/**
+ * WebSocket configuration class to set up WebSocket handlers.
+ */
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    /**
+     * Registers WebSocket handlers with the WebSocket handler registry.
+     *
+     * @param registry The WebSocket handler registry
+     */
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new MyWebSocketHandler(), "/ws/sessions/*")
+        registry.addHandler(new MyWebSocketHandler(), "/ws/sessions/{sessionId}")
                 .setAllowedOrigins("*");
     }
 }
